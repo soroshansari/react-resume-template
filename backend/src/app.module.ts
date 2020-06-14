4293021b7293e04fa77cb1653acf0fbde0aeb6a7
Soroosh Ansarimehr <sorosh.ansari@gmail.com>
2020-06-14 18:07:36 +0800

    deploy erverless backend completed

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { LanguagesController } from './languages.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, LanguagesController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
--- backend/src/models/language.ts
+++
@@ -1,6 +0,0 @@
-export class Language {
-  name: string;
-  constructor(name: string) {
-    this.name = name;
-  }
-}
